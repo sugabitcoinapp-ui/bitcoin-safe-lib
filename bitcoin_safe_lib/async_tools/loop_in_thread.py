97d88829faa9b25be2263be3cf48d239351328dc
andreasgriffin <andreasgriffin@proton.me>
2025-05-28 16:50:26 +0200

    fix for run_foreground

--- bitcoin_safe_lib/async_tools/loop_in_thread.py
+++ bitcoin_safe_lib/async_tools/loop_in_thread.py
@@ -1,3 +1,30 @@
+#
+# Bitcoin Safe
+# Copyright (C) 2024 Andreas Griffin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.html
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import asyncio
 import threading
 from collections.abc import Coroutine
@@ -27,9 +54,10 @@ class LoopInThread:
     def run_background(self, coro: Coroutine[Any, Any, _T]) -> Future[_T]:
         return asyncio.run_coroutine_threadsafe(coro, self.get_loop())
 
-    @staticmethod
-    def run_foreground(coro: Coroutine[Any, Any, _T]) -> _T:
-        return asyncio.run(coro)
+    def run_foreground(self, coro: Coroutine[Any, Any, _T]) -> _T:
+        """Schedule on the background loop and block until done."""
+        fut = self.run_background(coro)
+        return fut.result()
 
     def start(self) -> asyncio.AbstractEventLoop:
         """
